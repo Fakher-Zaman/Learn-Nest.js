7893e9a8e11d36cacb8249ae59700765cc3b7add
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-08-29 12:24:52 +0500

    Understanding Custom Controller

--- beginner-course/src/app.controller.ts
+++ beginner-course/src/app.controller.ts
@@ -1,46 +1,9 @@
-import { Controller, Delete, Get, Param, Patch, Post, Req } from "@nestjs/common";
-import { Request } from "express";
+import { Controller, Get } from "@nestjs/common";
 
-@Controller('/greet')
+@Controller()
 export class AppController {
-    // '/greet/'
     @Get()
-    getGreeting() {
-        return 'Welcome to the NestJS Main Controller'
-    }
-
-    // '/greet/user/'
-    @Get('/user')
-    getUsers() {
-        return {
-            name: "Fakher Zaman",
-            email: "fakherzamanofficial@gmail.com",
-        };
-    }
-
-    // '/greet/'
-    @Post()
-    store(@Req() req: Request) {
-        console.log(req.body);
-        return req.body;
-    }
-
-    // '/greet/:userId/'
-    @Patch('/:userId')
-    updateUser(@Req() req: Request) {
-        console.log(req.body);
-        return req.body;
-    }
-
-    // '/greet/:userId/'
-    @Get('/:userId')
-    getUser(@Param() params: {userId: number}) {
-        return params;
-    }
-
-    // '/greet/:userId/'
-    @Delete('/:userId')
-    deleteUser(@Param() params: {userId: number}) {
-        return params;
+    getMessage() {
+        return 'I am from main controller';
     }
 }
---
+++ beginner-course/src/user/user.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Delete, Get, Param, Patch, Post, Req } from "@nestjs/common";
+import { Request } from "express";
+
+@Controller('/user')
+export class UserController {
+    // '/user/'
+    @Get()
+    getMessage() {
+        return 'I am from user controller';
+    }
+
+    // '/user/users/'
+    @Get('/users')
+    getUsers() {
+        return {
+            name: "Fakher Zaman",
+            email: "fakherzamanofficial@gmail.com",
+        };
+    }
+
+    // '/user/'
+    @Post()
+    store(@Req() req: Request) {
+        console.log(req.body);
+        return req.body;
+    }
+
+    // '/user/:userId/'
+    @Patch('/:userId')
+    updateUser(@Req() req: Request) {
+        console.log(req.body);
+        return req.body;
+    }
+
+    // '/user/:userId/'
+    @Get('/:userId')
+    getUser(@Param() params: { userId: number }) {
+        return params;
+    }
+
+    // '/user/:userId/'
+    @Delete('/:userId')
+    deleteUser(@Param() params: { userId: number }) {
+        return params;
+    }
+}
