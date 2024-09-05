6b517144de6628759697426e42802c4435b498fa
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-09-05 14:32:33 +0500

    Understanding Passport Authentication

--- beginner-course/src/auth/auth.controller.ts
+++ beginner-course/src/auth/auth.controller.ts
@@ -1,21 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { UserService } from 'src/user/user.service';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private userService: UserService) {}
-
+    @UseGuards(AuthGuard('local'))
     @Post('/login')
-    async login(@Body() loginDto: any) {
-        const user = await  this.userService.findByEmail(loginDto.email);
-        if(user) {
-            if(user.password === loginDto.password) {
-                return user;
-            }
-
-            return 'Password incorrect';
-        }
-
-        return 'unauthenticated';
+    async login(@Request() req: any) {
+        return req.user;
     }
 }
