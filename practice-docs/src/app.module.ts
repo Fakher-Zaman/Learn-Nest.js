8f0be25b0d2416b37b59052b76fb21c97b38e206
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-09-09 10:51:16 +0500

    Modules in Nest.Js

--- practice-docs/src/app.module.ts
+++ practice-docs/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  controllers: [AppController],
+  providers: [AppService],
+  imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule { }
