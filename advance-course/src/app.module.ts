02fd3c49241519a1e71ee9061df91098d40f9b2b
Fakher-Zaman <fakherzamanofficial@gmail.com>
2024-09-16 14:12:17 +0500

    Middleware in Nest.js

--- advance-course/src/app.module.ts
+++ advance-course/src/app.module.ts
@@ -1,11 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { SongsController } from './songs/songs.controller';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(LoggerMiddleware).forRoutes('songs'); // option # 1
+    // consumer
+    //   .apply(LoggerMiddleware)
+    //   .forRoutes({
+    //     path: 'songs',
+    //     method: RequestMethod.POST
+    //   }); // option # 2
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController); // Option # 3
+  }
+}
